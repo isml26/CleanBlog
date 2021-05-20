4c40f3dba2a05c51bd548a2c820efd73b52d9ff2
unknown <iguven2697@gmail.com>
2021-05-20 18:50:41 +0300

    second

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 
 const express = require("express")
-
+const ejs = require("ejs")
 const app = express()
+app.use(express.json())
 
+app.set('view engine','ejs');
 
+//middleware is a function bewteen request and response
+app.use(express.static('public'))
 app.get("/",(req,res)=>{
-    const blog = { id: 1, title: "Blog title", description: "Blog description" }
-    res.send(blog)
+    res.render('index')
 })
+
+
+
+
+
+app.get("/about",(req,res)=>{
+    res.render('about')
+})
+
+app.get("/add",(req,res)=>{
+    res.render('add')
+})
+
 
 const PORT = process.env.PORT || 3000;
 
