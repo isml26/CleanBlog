3fbf5e6544b362aa811bc3b28814d5922115390a
unknown <iguven2697@gmail.com>
2021-05-20 19:58:11 +0300

    third

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 
 const express = require("express")
 const ejs = require("ejs")
-const app = express()
-app.use(express.json())
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const Post = require("./models/Post")
 
+const app = express()
+
+dotenv.config();
+app.use(express.urlencoded({extended:true}))//read data from url
+app.use(express.json())//convert data to json
 app.set('view engine','ejs');
 
 //middleware is a function bewteen request and response
 app.use(express.static('public'))
-app.get("/",(req,res)=>{
-    res.render('index')
+app.get("/",async (req,res)=>{
+    const posts = await Post.find({})
+
+    res.render('index',{
+        posts
+    })
 })
-
-
-
-
 
 app.get("/about",(req,res)=>{
     res.render('about')
@@ -24,6 +30,19 @@ app.get("/add",(req,res)=>{
     res.render('add')
 })
 
+
+app.post("/posts",async (req,res)=>{
+    await Post.create(req.body);
+    res.redirect('/')
+})
+
+
+
+mongoose.connect(process.env.URL, { useNewUrlParser: true, useUnifiedTopology: true});
+const connection = mongoose.connection;
+connection.once("open",()=>{
+  console.log("Connected Successfully");
+})
 
 const PORT = process.env.PORT || 3000;
 
