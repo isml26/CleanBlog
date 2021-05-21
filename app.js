bea3cfb7b86f77fec99506e7ec4dd684dc289680
unknown <iguven2697@gmail.com>
2021-05-21 17:09:47 +0300

    5th

--- app.js
+++ app.js
@@ -1,47 +1,46 @@
 
 const express = require("express")
-const ejs = require("ejs")
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
+const methodOverride = require('method-override');
 const Post = require("./models/Post")
-
+const controller = require("./controller/postController.js")
 const app = express()
 
 dotenv.config();
 app.use(express.urlencoded({extended:true}))//read data from url
 app.use(express.json())//convert data to json
 app.set('view engine','ejs');
+app.use(methodOverride('_method',{
+    methods:['POST','GET'],
+  }));
 
 //middleware is a function bewteen request and response
 app.use(express.static('public'))
-app.get("/",async (req,res)=>{
-    const posts = await Post.find({})
 
-    res.render('index',{
-        posts
-    })
-})
+app.get("/",controller.getAllPosts)
 
 app.get("/about",(req,res)=>{
     res.render('about')
 })
-
 app.get("/add",(req,res)=>{
     res.render('add')
 })
 
-app.get("/posts/:id",async (req,res)=>{
-    const post = await Post.findById(req.params.id)
-    res.render('post',{
-        post
-    })  
-});
+app.get("/posts/:id",controller.getPost);
 
-app.post("/posts",async (req,res)=>{
-    await Post.create(req.body);
-    res.redirect('/')
-})
+app.get('/posts/edit/:id', async (req, res) => {
+    const post = await Post.findOne({ _id: req.params.id });
+    res.render('edit', {
+      post,
+    });
+  });
 
+app.put('/posts/:id', controller.updatePost);
+
+app.post("/posts",controller.createPost)
+
+app.delete('/posts/:id',controller.deletePost)
 
 
 mongoose.connect(process.env.URL, { useNewUrlParser: true, useUnifiedTopology: true});
